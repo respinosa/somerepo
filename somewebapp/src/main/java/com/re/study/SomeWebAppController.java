ebe5cd404ba0cb1e1a90d9ef2b3a53293600ac43
r.espinosa.zambrano <r.espinosa.zambrano@CPX-EZN62E8YMYJ.dir.svc.accenture.com>
2017-09-25 23:24:23 -0500

    First commit in GitHub

---
+++ somewebapp/src/main/java/com/re/study/SomeWebAppController.java
@@ -0,0 +1,17 @@
+package com.re.study;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SomeWebAppController {
+	@RequestMapping(value = "/")
+	public String hello(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "hello";
+	}
+}
---
+++ somewebapp/src/main/java/com/re/study/entity/User.java
@@ -0,0 +1,81 @@
+package com.re.study.entity;
+
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name="item")
+@Access(AccessType.FIELD)
+public class User implements java.io.Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name="id",unique = true, nullable = false)
+	private Integer id;
+	
+	public Integer getId(){
+		return this.id;
+	}
+	
+	public void setId(Integer id){
+		this.id = id;
+	}
+	
+	@Column(name="first_name",nullable = false)
+	private String firstName;
+	
+	@Column(name="last_name",nullable = false)
+	private String lastName;
+	
+
+	@Column(name="email",nullable = false)
+	private String email;
+	
+	@Column(name="phone",nullable = false)
+	private String phone;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+
+
+}
